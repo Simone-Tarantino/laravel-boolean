59e54f3cfac5f9bae50eb842092a05ab1f2adc9c
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-17 18:10:00 +0100

    Stampa studenti da controller senza parte grafica

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,2 +1,10 @@
 @extends('layouts.layout')
 
+@section('main')
+@foreach ($students as $student)
+    <img src="{{$student['img']}}" alt="{{$student['name']}}">
+    <h2>{{$student['name']}} ({{$student['age']}} anni)</h2>
+    <h3>Assunt{{($student['genre'] == 'm') ? 'o' : 'a' }} da {{$student['where']}} come {{$student['role']}}</h3>
+    <p>{{$student['description']}}</p>
+    @endforeach
+@endsection
