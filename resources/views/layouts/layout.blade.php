dba665cbfbabbfe5a4c989d0dcfd5b6a89b891bd
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-17 17:05:41 +0100

    Creazione controller, view e base layout

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+<header>
+    @include('layouts.partials.header')
+</header>
+
+
+<main>
+    @yield('main')
+</main>
+
+
+<footer>
+    @include('layouts.partials.footer')
+</footer>
+        
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/students', 'StudentPageController@Index')->name('students');
+
+Route::get('/students/show', 'StudentPageController@Show')->name('students.show');
