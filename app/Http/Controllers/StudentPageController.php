c9ca811123046f343a4a0d8dba3e17a066627774
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-18 15:25:19 +0100

    Aggiornamento metodo controller

--- app/Http/Controllers/StudentPageController.php
+++ app/Http/Controllers/StudentPageController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 class StudentPageController extends Controller
 {
 
-    public function Index()
+    private $students;
+
+    function __construct()
     {
-        $students = [
+        $this->students = [
             [
                 'img' => 'https://www.boolean.careers/images/students/biagini.png',
                 'name' => 'Alessandro Biagini',
@@ -17,7 +19,7 @@ class StudentPageController extends Controller
                 'where' => 'DISC SPA',
                 'role' => 'web developer',
                 'description' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.',
-                'genre' => 'm'
+                'gender' => 'm'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/poggini.png',
@@ -26,7 +28,7 @@ class StudentPageController extends Controller
                 'where' => 'Prima Assicurazioni',
                 'role' => 'junior software engineer',
                 'description' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.',
-                'genre' => 'f'
+                'gender' => 'f'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/masetti.png',
@@ -35,7 +37,7 @@ class StudentPageController extends Controller
                 'where' => 'The Zen Agency',
                 'role' => 'web developer',
                 'description' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.',
-                'genre' => 'f'
+                'gender' => 'f'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/patruno.png',
@@ -44,7 +46,7 @@ class StudentPageController extends Controller
                 'where' => 'ArmadioVerde',
                 'role' => 'web developer',
                 'description' => 'Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End.',
-                'genre' => 'm'
+                'gender' => 'm'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/castellani.png',
@@ -53,7 +55,7 @@ class StudentPageController extends Controller
                 'where' => 'Pearch Digital',
                 'role' => 'data scientist',
                 'description' => 'Un passato da giocatore professionista di poker e una laurea in ingegneria gestionale alle spalle. Tommaso ha tramutato la sua passione per i numeri in un lavoro diventando data scientist a Dublino.',
-                'genre' => 'm'
+                'gender' => 'm'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/antidormi.png',
@@ -62,7 +64,7 @@ class StudentPageController extends Controller
                 'where' => 'Atoms Studio',
                 'role' => 'web developer',
                 'description' => 'Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.',
-                'genre' => 'm'
+                'gender' => 'm'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/ortenzi.png',
@@ -71,7 +73,7 @@ class StudentPageController extends Controller
                 'where' => 'RDS',
                 'role' => 'web developer',
                 'description' => 'Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.',
-                'genre' => 'f'
+                'gender' => 'f'
             ],
             [
                 'img' => 'https://www.boolean.careers/images/students/quarticelli.png',
@@ -80,95 +82,25 @@ class StudentPageController extends Controller
                 'where' => 'VLC 2',
                 'role' => 'web developer',
                 'description' => 'Per supportare la famiglia, Simone inizia a lavorare come operaio da giovanissimo. Fortemente motivato a cambiare vita, per 6 mesi non stacca mai lo sguardo dal traguardo, distinguendosi per tenacia e resilienza.',
-                'genre' => 'm'
+                'gender' => 'm'
             ]
         ];
+    }
+
+    public function Index()
+    {
+        $students = $this->students;
 
         return view('students', compact('students'));
     }
 
     public function Show($id)
     {
-        $students = [
-            [
-                'img' => 'https://www.boolean.careers/images/students/biagini.png',
-                'name' => 'Alessandro Biagini',
-                'age' => 25,
-                'where' => 'DISC SPA',
-                'role' => 'web developer',
-                'description' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.',
-                'genre' => 'm'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/poggini.png',
-                'name' => 'Paola Poggini',
-                'age' => 24,
-                'where' => 'Prima Assicurazioni',
-                'role' => 'junior software engineer',
-                'description' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.',
-                'genre' => 'f'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/masetti.png',
-                'name' => 'Loana Masetti ',
-                'age' => 36,
-                'where' => 'The Zen Agency',
-                'role' => 'web developer',
-                'description' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.',
-                'genre' => 'f'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/patruno.png',
-                'name' => 'Davide Patruno',
-                'age' => 29,
-                'where' => 'ArmadioVerde',
-                'role' => 'web developer',
-                'description' => 'Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End.',
-                'genre' => 'm'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/castellani.png',
-                'name' => 'Tommaso Castellani',
-                'age' => 30,
-                'where' => 'Pearch Digital',
-                'role' => 'data scientist',
-                'description' => 'Un passato da giocatore professionista di poker e una laurea in ingegneria gestionale alle spalle. Tommaso ha tramutato la sua passione per i numeri in un lavoro diventando data scientist a Dublino.',
-                'genre' => 'm'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/antidormi.png',
-                'name' => 'Nicholas Antidormi',
-                'age' => 20,
-                'where' => 'Atoms Studio',
-                'role' => 'web developer',
-                'description' => 'Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.',
-                'genre' => 'm'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/ortenzi.png',
-                'name' => 'Daniela Ortenzi',
-                'age' => 27,
-                'where' => 'RDS',
-                'role' => 'web developer',
-                'description' => 'Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.',
-                'genre' => 'f'
-            ],
-            [
-                'img' => 'https://www.boolean.careers/images/students/quarticelli.png',
-                'name' => 'Simone Quarticelli',
-                'age' => 32,
-                'where' => 'VLC 2',
-                'role' => 'web developer',
-                'description' => 'Per supportare la famiglia, Simone inizia a lavorare come operaio da giovanissimo. Fortemente motivato a cambiare vita, per 6 mesi non stacca mai lo sguardo dal traguardo, distinguendosi per tenacia e resilienza.',
-                'genre' => 'm'
-            ]
-        ];
-
-        if(!array_key_exists($id, $students)) {
+        if(!array_key_exists($id, $this->students)) {
             abort('404');
         }
 
-        $student = $students[$id];
+        $student = $this->students[$id];
 
         return view('show', compact('student'));
     }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,6 @@
 @section('main')
     <img src="{{$student['img']}}" alt="{{$student['name']}}">
     <h2>{{$student['name']}} ({{$student['age']}} anni)</h2>
-    <h3>Assunt{{($student['genre'] == 'm') ? 'o' : 'a' }} da {{$student['where']}} come {{$student['role']}}</h3>
+    <h3>Assunt{{($student['gender'] == 'm') ? 'o' : 'a' }} da {{$student['where']}} come {{$student['role']}}</h3>
     <p>{{$student['description']}}</p>
 @endsection
